bddaf0a6691cfa02c37a9e81004be31852b466ab
mbaghel <michael.baghel@gmail.com>
2019-01-30 21:34:03 +0400

    added db connection

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,35 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
+var expect = require("chai").expect;
+var MongoClient = require("mongodb");
 
-module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+const CONNECTION_STRING = process.env.DB;
+let messageDb;
+MongoClient.connect(
+  CONNECTION_STRING,
+  (err, db) => {
+    if (err) {
+      console.log(err);
+    }
+    messageDb = db;
+  }
+);
 
+module.exports = function(app) {
+  app.route("/api/threads/:board").post(function(req, res) {
+    const board = req.params.board;
+    const text = req.body.text;
+    const deletePass = req.body.delete_password;
+
+    res.end();
+  });
+  app.route("/api/replies/:board");
 };
